032f08e7d400d9cf22270cef7470935da83e2745
Jyoti prakash <jyoti.astomverse@gmail.com>
2026-03-15 16:03:54 +0530

    Clock_demo

---
+++ Clock/clock_demo_tb.v
@@ -0,0 +1,17 @@
+`timescale 1ps/1ps
+`include "clock_demo.v"
+
+module clock_demo_tb();
+output clk;
+my_clock mut(clk);
+
+initial begin
+   #100
+$finish; 
+end
+ 
+ initial begin
+    $dumpfile("clock_demo.vcd");
+    $dumpvars(0,clock_demo_tb);
+ end
+endmodule
